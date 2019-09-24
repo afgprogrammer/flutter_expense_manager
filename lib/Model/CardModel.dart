9a0d5c3f77961bb11a0c569c7cc35ae737252014
Mohammad Rahmani <rto1680@gmail.com>
2019-09-24 18:15:47 +0430

    Make a model for cards

---
+++ lib/Model/CardModel.dart
@@ -0,0 +1,12 @@
+class CardModel {
+  
+  final int id;
+  final String name;
+  final String bankName;
+  final String number;
+  final String currency;
+
+  final int available;
+
+  CardModel({this.id, this.name, this.bankName, this.number, this.currency, this.available});
+}
